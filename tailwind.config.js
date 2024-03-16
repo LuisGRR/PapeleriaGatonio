8d4880c5bc4bfd4557a9c000e169868b5390360c
LuisGRR <lgerardo685@gmail.com>
2024-03-16 16:59:10 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "science-blue": {
+        50: "#ecfaff",
+        100: "#d4f2ff",
+        200: "#b2ebff",
+        300: "#7de0ff",
+        400: "#40cbff",
+        500: "#14acff",
+        600: "#008bff",
+        700: "#0073ff",
+        800: "#0062d9",
+        900: "#0850a0",
+        950: "#0a3161",
+      },
+      orange: "#F6995C",
+      blue: "#51829B",
+      aqua: "#9BB0C1",
+      beige: "#EADFB4",
+    },
+  },
+  plugins: [],
+};
