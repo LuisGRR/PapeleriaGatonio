c51517394b78b8d7464cf27db1737ba35dfaff78
LuisGRR <lgerardo685@gmail.com>
2024-03-17 16:50:53 -0600

    Correciones y mejoras de estilos

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-// import './style.css'
+ import './style.css';
 
-// window.addEventListener('scroll', function() {
-//     const parallax = document.getElementById('parallax');
-//     let scrollPosition = window.pageYOffset;
-  
-//     parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
-//   });
+ if(localStorage.getItem('consentimientoCookies') === 'rechazado' || !localStorage.getItem('consentimientoCookies')){
+    document.getElementById('avisoCokkies').style.display = 'block';
+ }
+ 
+document.getElementById('aceptCookie').addEventListener('click',function(){
+    // console.log('acepte cookkies');
+     // Guarda el consentimiento del usuario en el almacenamiento local
+    //  gtag('consent', 'update', {
+    //     'ad_storage': 'granted',
+    //     'analytics_storage': 'granted'
+    //   });
+     localStorage.setItem('consentimientoCookies', 'aceptado');
+
+    document.getElementById('avisoCokkies').style.display = 'none';
+ });
+
+ document.getElementById('closecookie').addEventListener('click',function(){
+    // console.log('close cookie');
+    // gtag('consent', 'update', {
+    //     'ad_storage': 'denied',
+    //     'analytics_storage': 'denied'
+    //   });
+    // Guarda el consentimiento del usuario en el almacenamiento local
+    localStorage.setItem('consentimientoCookies', 'rechazado');
+
+    // Oculta el aviso de cookies
+    document.getElementById('avisoCokkies').style.display = 'none';
+ });
--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       },
     },
   },
-  base: "/papeleria-gatonio/",
+  base: "/PapeleriaGatonio/",
 });
