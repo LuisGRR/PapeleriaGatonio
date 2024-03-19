78c14ad18145a7ce605499a2645d9231fc4f619e
LuisGRR <lgerardo685@gmail.com>
2024-03-18 22:46:54 -0600

    Animacion optimizasion de carga de imagenes

--- main.js
+++ main.js
@@ -1,30 +1,60 @@
- import './style.css';
+import "./style.css";
 
- if(localStorage.getItem('consentimientoCookies') === 'rechazado' || !localStorage.getItem('consentimientoCookies')){
-    document.getElementById('avisoCokkies').style.display = 'block';
- }
- 
-document.getElementById('aceptCookie').addEventListener('click',function(){
-    // console.log('acepte cookkies');
-     //Guarda el consentimiento del usuario en el almacenamiento local
-     gtag('consent', 'update', {
-        'ad_storage': 'granted',
-        'analytics_storage': 'granted'
-      });
-     localStorage.setItem('consentimientoCookies', 'aceptado');
+import "animate.css";
 
-    document.getElementById('avisoCokkies').style.display = 'none';
- });
+if (
+  localStorage.getItem("consentimientoCookies") === "rechazado" ||
+  !localStorage.getItem("consentimientoCookies")
+) {
+  document.getElementById("avisoCokkies").style.display = "block";
+}
 
- document.getElementById('closecookie').addEventListener('click',function(){
-    // console.log('close cookie');
-    gtag('consent', 'update', {
-        'ad_storage': 'denied',
-        'analytics_storage': 'denied'
-      });
-    // Guarda el consentimiento del usuario en el almacenamiento local
-    localStorage.setItem('consentimientoCookies', 'rechazado');
+document.getElementById("aceptCookie").addEventListener("click", function () {
+  console.log("acepte cookkies");
+  //   Guarda el consentimiento del usuario en el almacenamiento local
+  gtag("consent", "update", {
+    ad_storage: "granted",
+    analytics_storage: "granted",
+  });
+  localStorage.setItem("consentimientoCookies", "aceptado");
 
-    // Oculta el aviso de cookies
-    document.getElementById('avisoCokkies').style.display = 'none';
- });
+  let avisoCokkies = document.getElementById("avisoCokkies");
+
+  avisoCokkies.classList.add("animate__backOutRight");
+
+  avisoCokkies.addEventListener("animationend", function () {
+    avisoCookies.style.display = "none";
+  });
+});
+
+document.getElementById("closecookie").addEventListener("click", function () {
+  // console.log('close cookie');
+  gtag("consent", "update", {
+    ad_storage: "denied",
+    analytics_storage: "denied",
+  });
+  // Guarda el consentimiento del usuario en el almacenamiento local
+  localStorage.setItem("consentimientoCookies", "rechazado");
+
+  let avisoCookies = document.getElementById("avisoCokkies");
+
+  avisoCookies.classList.add("animate__backOutRight");
+  // Oculta el aviso de cookies
+  //  document.getElementById('avisoCokkies').style.display = 'none';
+  avisoCokkies.addEventListener("animationend", function () {
+    avisoCookies.style.display = "none";
+  });
+});
+
+let animationLogo = document.getElementById("logoAnimation");
+
+if (animationLogo) {
+  animationLogo.addEventListener("mouseover", function () {
+    animationLogo.classList.add("animate__headShake");
+  });
+
+  animationLogo.addEventListener("mouseout", function () {
+    animationLogo.classList.remove("animate__headShake");
+    animationLogo.classList.remove("animate__bounce");
+  });
+}
